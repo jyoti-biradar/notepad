95f98699a9104a3625a79c9adf84cb0522a211dc
Akshata Bhosale <bhosale.akshata99@gmail.com>
2019-05-07 15:46:01 +0530

    second project notepad

---
+++ src/notepadapplication/NotepadApplication.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package notepadapplication;
+
+/**
+ *
+ * @author Felix-ITs
+ */
+public class NotepadApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      notepad note=new notepad();
+    }
+    
+}
